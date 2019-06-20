288731f8472b84cfd6a9054bce5b10de2eede2ce
Himani Agarwal <40848128+Himani200@users.noreply.github.com>
2019-06-20 06:09:05 +0000

    Add files via upload
    Create a numpy array of 8x2 having number in each cell b/w 100 & 200 such that the difference b/w each element is 5.

---
+++ 2ml.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+'''#a=np.arange(100,125,1)
+i=100
+for i in 25:
+	k=i+80
+	k1=str(k)
+	j=k1
+        b=np.arange(i,j,5).reshape(8,2)
+        print("\n")
+        print(b)	
+	i=i+1       
+'''
+i=100
+for l in range(26):
+     j=i+80
+     b=np.arange(i,j,5).reshape(8,2)
+     print("\n")     
+     print(b)
+     i=i+1
+   
