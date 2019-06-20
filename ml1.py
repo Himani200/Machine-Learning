bcd2d55117e84b6c11e2ca40b29f0fe77cae3657
Himani Agarwal <40848128+Himani200@users.noreply.github.com>
2019-06-20 05:39:22 +0000

    Update ml1.py

--- ml1.py
+++ ml1.py
@@ -9,7 +9,8 @@ b=int(input("enter the no. of columns for array-1:"))
 a1=int(input("enter the no. of rows for array-2: "))
 b1=int(input("enter the no. of columns for array-2:"))
 
-x=str(np.random.randint(low=0,high=100,size=(a,b)))
+x=str(np.random.randint(low=0,high=100,size=(a,b)))  
+#random.randint is to generate random integers and low and high are the lower and upper limit for the integers
 print(x)
 f=open('fml1.txt','a')
 f.write(x)
