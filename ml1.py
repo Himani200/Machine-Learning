337da3afe60e9555650e6c72c26bded59b94ff25
Himani Agarwal <40848128+Himani200@users.noreply.github.com>
2019-06-20 05:29:21 +0000

    Add files via upload
    Create 2D numpy : Take the dimensions from the user, fill it with random number and store the array in the file.

---
+++ ml1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import  numpy as np
+
+print("Enter the size of the matrix")
+a=int(input("enter the no. of rows for array-1: "))
+b=int(input("enter the no. of columns for array-1:"))
+
+a1=int(input("enter the no. of rows for array-2: "))
+b1=int(input("enter the no. of columns for array-2:"))
+
+x=str(np.random.randint(low=0,high=100,size=(a,b)))
+print(x)
+f=open('fml1.txt','a')
+f.write(x)
+f.write('\n')
+f.close()
+print("\n")
+y=str(np.random.randint(low=0,high=100,size=(a1,b1)))
+print(y)         
+f=open('fml11.txt','a')
+f.write(y)
+f.write('\n')
+f.close()                                                              
